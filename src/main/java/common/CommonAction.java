53a69790ac45ef4f998e735b61e3b7bfc54624ed
Mohammad Uddin <mtaslimu010@gmail.com>
2023-06-22 17:14:57 -0400

    update sent to new branch

--- src/main/java/common/CommonAction.java
+++ src/main/java/common/CommonAction.java
@@ -1,5 +1,11 @@
 package common;
 
+import org.openqa.selenium.WebElement;
+
 public class CommonAction {
+	
+	public static String getInnerHTML(WebElement element) {
+		return element.getAttribute("innerHTML");
+	}
 
 }
--- src/main/java/util/Configuration.java
+++ src/main/java/util/Configuration.java
@@ -1,5 +1,33 @@
 package util;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Configuration {
 
+	Properties properties;
+
+	public Configuration() {
+		loadProperties();
+	}
+
+	private void loadProperties() {
+		try {
+			properties = new Properties();
+			InputStream iStream = getClass().getClassLoader().getResourceAsStream("configuration.properties");
+			properties.load(iStream);
+		} catch (Throwable e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+	public String readProp(String key) {
+		return properties.getProperty(key);
+		
+	}
+	
+	public int readPropNum(String key) {
+		return Integer.parseInt(readProp(key));
+	}
 }
